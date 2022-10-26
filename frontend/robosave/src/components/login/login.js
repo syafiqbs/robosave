ffc6d49b00e9469c6d94dd080cce7a23e0bab24d
jalaluddins2020 <jalaluddins.2020@scis.smu.edu.sg>
2022-10-26 15:01:19 +0800

    log in page styled

--- frontend/robosave/src/components/login/login.js
+++ frontend/robosave/src/components/login/login.js
@@ -1,14 +1,46 @@
 import React from 'react';
 
 import styles from './login.module.css';
-import { Text } from '@chakra-ui/react'
+import { Center, Text, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Box, Button } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
+import blackLogo from "../../assets/black-logo.png";
 
 class Login extends React.Component {
   render() {
     return (
       <div className={styles.body}>
-        <Text fontSize='3xl' as='b'>Welcome to Robosave</Text>
+        <Grid
+          h='800px'
+          templateColumns='repeat(11, 1fr)'
+          gap={4}
+        >
+          <GridItem colSpan={6} className={styles.main}>
+            <Center>
+              <Box m={40}>
+                <img src={blackLogo} alt="" width="250"></img>                            
+                <Center>
+                  <Text fontSize='5xl' fontWeight="bold" mt={2}>robosave</Text>
+                </Center>
+              </Box>
+            </Center>
+          </GridItem>
+          <GridItem colSpan={5}>
+            <Box mx={20} mt={40}>
+              <Text fontSize='3xl' fontWeight="semibold">Sign In</Text>
+              <Text fontSize='l' mt={1} color="gray.400">Enter your Username and Pin to sign in!</Text>
+            </Box>
+            <FormControl isRequired mx={20} my={5} width="auto">
+              <FormLabel >Username</FormLabel>
+              <Input mb={5} placeholder='Username'/>
+              <FormLabel>PIN</FormLabel>
+              <Input mb={5} placeholder='6 Digit PIN'/>
+              <Button colorScheme='green' mt={5} variant='solid' w="100%" bg='green.400'>
+                Sign In
+              </Button>
+            </FormControl>
+          </GridItem>
+        </Grid>
       </div>
     )
   }
