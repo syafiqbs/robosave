44d6cf9b8a0b29c80f7d138659486a3925685b69
Jing Yi Yeo <jingyi.yeo.2020@scis.smu.edu.sg>
2022-11-11 19:33:14 +0800

    billingorg

---
+++ backend/getBillingOrganisations.py
@@ -0,0 +1,25 @@
+import requests, json
+from functions import url
+
+def getBillingOrganizations():
+    serviceName = 'getBillingOrganizations'
+    headerObj = {
+                        'Header': {
+                        'serviceName': serviceName,
+                        'userID': '',
+                        'PIN': '',
+                        'OTP': ''
+                        }
+                        }
+    final_url="{0}?Header={1}".format(url(),json.dumps(headerObj))
+    response = requests.post(final_url)
+    
+    types = response.json()['Content']['ServiceResponse']['BillingOrgList']['BillingOrg']
+    org={}
+    
+    for i in range(len(types)):
+        billingOrg = types[i]
+        org[billingOrg['BillingOrgName']]= billingOrg['AccountID']
+    return org
+
+print(getBillingOrganizations())
